841af321bf2f4defc13bd24fbdc51c7a0dffb7dd
betsybaileyy <betsy.bailey80@gmail.com>
2020-05-20 17:28:05 -0400

    Updates

---
+++ precache-manifest.51a4dc7adb3bf4cda2e91dc76e8edbd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c40853178700590f0446095184d568be",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "4ade22d99db272ef2f1c",
+    "url": "/react-project/static/css/main.10e71321.chunk.css"
+  },
+  {
+    "revision": "00788b6e6b58fbab2fa1",
+    "url": "/react-project/static/js/2.00164ff7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-project/static/js/2.00164ff7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ade22d99db272ef2f1c",
+    "url": "/react-project/static/js/main.42ac21b3.chunk.js"
+  },
+  {
+    "revision": "26c467187b2b6587f50e",
+    "url": "/react-project/static/js/runtime-main.a7393182.js"
+  }
+]);
